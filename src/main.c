324863fe9e5d629d658dce6501d3aacad40348a0
Célestin Marot <celestin.marot@uclouvain.be>
2020-02-12 18:01:17 +0100

    updating colormap

--- src/main.c
+++ src/main.c
@@ -14,9 +14,14 @@ static void colormap(float v, float color[3])
 	float v2 = 1.25*v;
 	float v3 = fminf(0.5,v)*2.0;
 
-	color[0] = 0.8f*(-v1*v1+1.0f);
-	color[1] = 0.8f*(6.0f*v2*v2*(1.0f-v2));
-	color[2] = 0.8f*(5.5f*v3*(1.0f-v3)*(1.0f-v3));
+	color[0] = -v1*v1+1.0f;
+	color[1] = 6.0f*v2*v2*(1.0f-v2);
+	color[2] = 5.5f*v3*(1.0f-v3)*(1.0f-v3);
+
+	/* alternative: classical jet colormap */
+	// color[0] = 1.5 - 4.0 * fabs(v - 0.75);
+	// color[1] = 1.5 - 4.0 * fabs(v - 0.5 );
+	// color[2] = 1.5 - 4.0 * fabs(v - 0.25);
 }
 
 
