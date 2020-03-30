4ed7a40c960653a8b752a741e83ac0ba015a3028
Jimmy Fraiture <jimmy.fraiture@student.uclouvain.be>
2020-03-30 12:16:17 +0200

    Add Nicolas's part

--- src/consistency.c
+++ src/consistency.c
@@ -27,3 +27,32 @@ xy* correct_grad(xy *current_grad, Particle *p, double kh, Kernel kernel){
 
 	return xy_new((m22*current_grad->x - m21*current_grad->y)/det, (-m12*current_grad->x + m22*current_grad->y)/det);
 }
+
+void Corrective_Smoothed_Particle_Method(Particle *p,Particle_derivatives *dp, Setup *setup){
+    double num1=0.0;
+    double denom1=0.0;
+    double denom2=0.0;
+    double denom3=0.0;
+        //Correction of the density of our particle.
+    ListNode *current = p->neighborhood->head;
+    while(current != NULL){
+        Particle *j = current->v;
+        num1 += j->rho*eval_kernel(p->pos,j->pos,setup->kh,setup->kernel)*j->m/j->rho;
+        denom1 += eval_kernel(p->pos,j->pos,setup->kh,setup->kernel)*j->m/j->rho;
+        current = current->next;
+    }
+
+    //p->rho->x = num1/denom1;
+        //Correction of the gradient of pressure of our particle
+    
+    current = p->neighborhood->head;
+    while(current != NULL){
+        Particle *j = current->v;
+        denom2+=eval_kernel(p->pos,j->pos,setup->kh,XXX)*(j->pos->x-p->pos->x)*j->m/j->rho;
+        denom3+=eval_kernel(p->pos,j->pos,setup->kh,XXX)*(j->pos->y-p->pos->y)*j->m/j->rho;
+        current = current->next;
+        
+    }
+    dp->grad_P->x = dp->grad_P->x/denom2;
+    dp->grad_P->y = dp->grad_P->y/denom3;
+}
